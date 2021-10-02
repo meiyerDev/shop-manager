0a853ce86bb21a226cfba037ad65941020c2af82
Meiyer Jaimes <meyerjaimes123@gmail.com>
2021-10-02 13:50:09 -0400

    [ADD] custom handler from exceptions render

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponse;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -37,5 +46,74 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+
+            return $this->errorResponse(
+                $message,
+                $code
+            );
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $model = class_basename($exception->getModel());
+
+            return $this->errorResponse(
+                "Does not exist any instance of {$model} with the given id",
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse(
+                $exception->getMessage(),
+                Response::HTTP_FORBIDDEN
+            );
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return $this->errorResponse(
+                $exception->getMessage(),
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        if ($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->errorResponse(
+                $errors,
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        // MOSTRAR CODIGO EN DESARROLLO
+        if (config('app.debug', false)) {
+            return parent::render($request, $exception);
+        }
+
+        if ($request->wantsJson()) {
+            parent::render($request, $exception);
+
+            return $this->errorResponse(
+                "Enexpected error, Try later",
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        return parent::render($request, $exception);
     }
 }
